5b7c662eaef5735a3a9c19f1e7c2723941a75f43
Asgeir Bjørgan <asgeirbj@samfundet.no>
2020-02-15 19:59:11 +0100

    Renew db connection, show autorefreshing error page on errors (avoid eternal Internal server error on database server downtime, as experienced sometimes).

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 import psycopg2
 import qsostats
 import configparser
@@ -17,33 +17,47 @@ db_config = configparser.ConfigParser()
 db_config.read(db_config_path)
 db_config = db_config['db_config']
 
-#database connection
-db_connection = psycopg2.connect(dbname=db_config['dbname'],
-                                 user=db_config['user'],
-                                 password=db_config['password'],
-                                 host=db_config['hostname'],
-                                 port=db_config['port'])
+def get_db():
+    if 'db' not in g:
+        g.db = psycopg2.connect(dbname=db_config['dbname'],
+                                         user=db_config['user'],
+                                         password=db_config['password'],
+                                         host=db_config['hostname'],
+                                         port=db_config['port'])
+    return g.db
+
+@app.teardown_appcontext
+def close_db(error):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
 
 @app.route("/")
 def index():
+    db_connection = get_db()
+
     #counts
     count_today = qsostats.qso_count(db_connection, 1)[0]
     count_twomonths = qsostats.qso_count(db_connection, 2*31)[0]
 
     #rate
     rate = qsostats.current_qso_rate(db_connection)
-    print(rate)
 
     #collect in struct
     qsostats_dict = {'count_today': count_today,
-                'count_twomonths': count_twomonths,
-                'rate': rate}
+                    'count_twomonths': count_twomonths,
+                    'rate': rate}
 
 
     #get last QSOs
     last_qsos_list = qsostats.last_qsos(db_connection)
 
     return render_template('index.html', qsostats=qsostats_dict, last_qsos=last_qsos_list)
+
+@app.errorhandler(Exception)
+def errorhandler(error):
+    return render_template('error.html', error_type=type(error))
 
 if __name__ == "__main__":
     app.run()
